2be67cd94887f25dfe556f7258afba06c9859b97
automata <automatalab.contacto@gmail.com>
2023-10-02 16:24:04 -0500

    product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,15 +25,13 @@ router.get('/:id', async (req, res) => {
 
 
 
-  // This route uses async/await with try/catch for errors
-  // along with HTTP status codes
   router.post('/', async (req, res) => {
     try {
       const newCategory = await Category.create(req.body);
-      // 200 status code means the request is successful
+
       res.status(200).json(newCategory);
     } catch (err) {
-      // 400 status code means the server could not understand the request
+
       res.status(400).json(err);
     }
   });
