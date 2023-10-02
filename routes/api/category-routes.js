a7d10de7e9d178d88d9526e9f0859d55d19c8ad9
automata <automatalab.contacto@gmail.com>
2023-10-02 12:29:39 -0500

    categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,41 +3,77 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  Category.findAll()
-  .then(categories => {
-    res.json(categories)
-  }).catch(error => {
-    res.status(500).json(error)
-  }) 
+router.get('/', async (req, res) => {
+  const categories = await Category.findAll().catch((err) => {
+    res.json(err);
+  });
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  Category.findOne({
-    where: {
-      id: req.params.id,
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryId = await Category.findByPk(req.params.id);
+    if (!categoryId) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
     }
-  }).then(category => {
-    res.json(category)
-  }).catch(error => {
-    res.status(500).json(error)
-  })
+    res.status(200).json(categoryId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+
+  // This route uses async/await with try/catch for errors
+  // along with HTTP status codes
+  router.post('/', async (req, res) => {
+    try {
+      const newCategory = await Category.create(req.body);
+      // 200 status code means the request is successful
+      res.status(200).json(newCategory);
+    } catch (err) {
+      // 400 status code means the server could not understand the request
+      res.status(400).json(err);
+    }
+  });
+
+
+
+// UPDATE a user
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
